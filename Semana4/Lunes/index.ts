7e0fe8e7a5f610429b29af9c3679e095a3c01f3c
Guille <guilleriofrio12@gmail.com>
2025-10-06 16:27:15 +0200

    Primera parte, Axios y Async

--- Semana4/Lunes/index.ts
+++ Semana4/Lunes/index.ts
@@ -1,1 +1,30 @@
 import axios from "axios"
+
+axios.get("https://rickandmortyapi.com/api/character/1").then((res) => {
+    //console.log(res.data)
+})
+
+/*const getCharacter = async (id: number) => {
+    const personaje = await axios.get("https://rickandmortyapi.com/api/character/" + id);
+    //console.log(personaje);
+    return personaje.data;
+}*/
+
+//Async es una funcion la cual se ejecuta en un hilo aparte
+//Await es lo que recibe la promesa(una fachada para un then)
+const getCharacterTheRightWay = async (id: number) => {
+    try {
+        const response = await axios.get("https://rickandmortyapi.com/api/character/"+id);
+        return response.data;
+    }catch(error){
+        if(axios.isAxiosError(error)){
+            console.log("Axios error" + error.message);
+        }else{
+            console.log("Error" + error);
+        }
+    }
+}
+
+const personaje = await (getCharacterTheRightWay(2));
+
+console.log(personaje);
